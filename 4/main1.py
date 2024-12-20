7ad12daec0448af53fb2704bc034a4cf83ef3326
Raul Viana <raul.m.viana@inesctec.pt>
2024-12-20 16:23:58 +0000

    solved day4 part1

--- 4/main1.py
+++ 4/main1.py
@@ -5,7 +5,7 @@ back_target = 'SAMX'
 def main():
     file_content = []
 
-    with open('./4/input.txt', 'r') as file:
+    with open('input.txt', 'r') as file:
         for line in file:
             line = line.replace('\n', '')
             temp_line = []
@@ -19,9 +19,10 @@ def main():
     for i in range(length):
         for j in range(height):
             if file_content[i][j] == target[0]:
-                total += parse(i, j, length, height, file_content)
-        break
+                total += parse(i, j, file_content)
+
     print(f"Total: {total}")
+
 
 def check_return_value(return_value):
     if return_value == target or return_value == back_target:
@@ -29,34 +30,40 @@ def check_return_value(return_value):
     else:
         return 0
     
-def parse(i, j, length, height, file_content):
+def parse(i, j, file_content):
     # j -> horizontal
     # i -> vertical
-    target_length = len(target) -1
     sub_total = 0
-    if j < (length - target_length):
-        return_value = go_forward(i, j, 0, file_content, '')
-        print(f"Return Value: {return_value}, {check_return_value(return_value)}")
-        sub_total = check_return_value(return_value)
-    #if j > target_length:
-    #    return_value += go_back(i, j, target, 0, file_content)
-    #if i > target_length:
-    #    return_value += go_up(i, j, target, 0, file_content)
-    #if i < (height - target_length):
-    #    return_value += go_down(i, j, target, 0, file_content)
-    #if j < (length - target_length) and i > target_length:
-    #    return_value += go_forward_up(i, j, target, 0, file_content)
-    #if j < (length - target_length) and i < (height - target_length):
-    #    return_value += go_forward_down(i, j, target, 0, file_content)
-    #if j > target_length and i > target_length:
-    #    return_value += go_back_up(i, j, target, 0, file_content)
-    #if j > target_length and i < (height - target_length):
-    #    return_value += go_back_down(i, j, target, 0, file_content)
-
+    return_value = ''
+    return_value = go_forward(i, j, 0, file_content, '')
+    sub_total += check_return_value(return_value)
+    return_value = ''
+    return_value = go_back(i, j, target, 0, file_content, '')
+    sub_total += check_return_value(return_value)
+    return_value = ''
+    return_value = go_up(i, j, target, 0, file_content, '')
+    sub_total += check_return_value(return_value)
+    return_value = ''
+    return_value = go_down(i, j, target, 0, file_content, '')
+    sub_total += check_return_value(return_value)
+    return_value = ''
+    return_value = go_forward_up(i, j, target, 0, file_content, '')
+    sub_total += check_return_value(return_value)
+    return_value = ''
+    return_value = go_forward_down(i, j, target, 0, file_content, '')
+    sub_total += check_return_value(return_value)
+    return_value = ''
+    return_value = go_back_up(i, j, target, 0, file_content, '')
+    sub_total += check_return_value(return_value)
+    return_value = ''
+    return_value = go_back_down(i, j, target, 0, file_content, '')
+    sub_total += check_return_value(return_value)
     return sub_total
 
 
 def go_forward(i, j, position, file_content, return_value):
+    if j > len(file_content[0]) - 1:
+        return return_value
     return_value += file_content[i][j]
     if file_content[i][j] == target[position]:
         if position == len(target) -1:
@@ -67,68 +74,104 @@ def go_forward(i, j, position, file_content, return_value):
         return return_value
     
 
-def go_back(i, j, target, position, file_content):
+def go_back(i, j, target, position, file_content, return_value):
+    if j < 0:
+        return return_value
+    return_value += file_content[i][j]
     if file_content[i][j] == target[position]:
         if position == len(target) -1:
-            return 1
+            return return_value
         else:
-            return go_back(i, j - 1, target, position + 1, file_content)
+                return go_back(i, j - 1, target, position + 1, file_content, return_value)
     else:
-        return 0
+        return return_value
 
-def go_up(i, j, target, position, file_content):
+def go_up(i, j, target, position, file_content, return_value):
+    return_value += file_content[i][j]
+    if i < 0:
+        return return_value
     if file_content[i][j] == target[position]:
         if position == len(target) -1:
-            return 1
+            return return_value
         else:
-            return go_up(i - 1, j, target, position + 1, file_content)
+                return go_up(i - 1, j, target, position + 1, file_content, return_value)
     else:
-        return 0
+        return return_value
 
-def go_down(i, j, target, position, file_content):
+def go_down(i, j, target, position, file_content, return_value):
+    if i > len(file_content) - 1:
+        return return_value
+    return_value += file_content[i][j]
     if file_content[i][j] == target[position]:
         if position == len(target) -1:
-            return 1
+            return return_value
         else:
-            return go_down(i + 1, j, target, position + 1, file_content)
+            try:
+                return go_down(i + 1, j, target, position + 1, file_content, return_value)
+            except IndexError:
+                return return_value
     else:
-        return 0
+        return return_value
 
-def go_forward_up(i, j, target, position, file_content):
+def go_forward_up(i, j, target, position, file_content, return_value):
+    if i < 0 or j > len(file_content[0]) - 1:
+        return return_value
+    return_value += file_content[i][j]
     if file_content[i][j] == target[position]:
         if position == len(target) -1:
-            return 1
+            return return_value
         else:
-            return go_forward_up(i - 1, j + 1, target, position + 1, file_content)
+            try:
+                return go_forward_up(i - 1, j + 1, target, position + 1, file_content, return_value)
+            except IndexError:
+                return return_value
     else:
-        return 0
+        return return_value
 
-def go_forward_down(i, j, target, position, file_content):
+def go_forward_down(i, j, target, position, file_content, return_value):
+    if i > len(file_content) - 1 or j > len(file_content[0]) - 1:
+        return return_value
+    return_value += file_content[i][j]
     if file_content[i][j] == target[position]:
         if position == len(target) -1:
-            return 1
+            return return_value
         else:
-            return go_forward_down(i + 1, j + 1, target, position + 1, file_content)
+            try:
+                return go_forward_down(i + 1, j + 1, target, position + 1, file_content, return_value)
+            except IndexError:
+                return return_value
     else:
-        return 0
+        return return_value
 
-def go_back_up(i, j, target, position, file_content):
+def go_back_up(i, j, target, position, file_content, return_value):
+    if i < 0 or j < 0:
+        return return_value
+    return_value += file_content[i][j]
     if file_content[i][j] == target[position]:
         if position == len(target) -1:
-            return 1
+            return return_value
         else:
-            return go_back_up(i - 1, j - 1, target, position + 1, file_content)
+            try:
+                return go_back_up(i - 1, j - 1, target, position + 1, file_content, return_value)
+            except IndexError:
+                return return_value
     else:
-        return 0
+        return return_value
 
-def go_back_down(i, j, target, position, file_content):
+def go_back_down(i, j, target, position, file_content, return_value):
+    if i > len(file_content) - 1 or j < 0:
+        return return_value
+    return_value += file_content[i][j]
     if file_content[i][j] == target[position]:
         if position == len(target) -1:
-            return 1
+            return return_value
         else:
-            return go_back_down(i + 1, j - 1, target, position + 1, file_content)
+            try:
+                return go_back_down(i + 1, j - 1, target, position + 1, file_content, return_value)
+            except IndexError:
+                return return_value
     else:
-        return 0
+        return return_value
 
 
 if __name__ == "__main__":
